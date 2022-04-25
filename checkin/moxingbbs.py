b989d4da7eab19068318e0add7fc2ad2a37fe18b
tanmx <tanming@gmail.com>
2022-04-25 18:00:31 +0800

    add

---
+++ checkin/moxingbbs.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+import requests
+import re
+import json
+import logging
+import os
+import hashlib
+import push
+'''
+cron: 51 8 * * * moxingbbs.py
+new Env('MX论坛签到');
+'''
+#https://blog.homurax.com/2020/01/13/discuz-login/
+#https://gist.github.com/ficapy/11130233
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger("MX论坛")
+
+URL = 'https://moxing.institute'
+
+class MXcheckin:
+    def __init__(self, username, password):
+        self.session = requests.session()
+        self.url = URL
+        self.username = username
+        self.password = password
+
+    def md5(self, password):
+        return hashlib.md5(password.encode(encoding='UTF-8')).hexdigest()
+
+    def user_login(self):
+        password = self.md5(self.password)
+        #先获取 loginhash 和 formhash
+        loginhash, formhash = self.form_hash()
+        login_url = self.url + '/member.php?mod=logging&action=login&loginsubmit=yes&frommessage&loginhash=' + loginhash + '&inajax=1'
+        fromData = {
+            'formhash': formhash,
+            'referer': URL,
+            'loginfield': 'username',
+            'username': self.username,
+            'password': password,
+            'questionid': 0,
+            'answer': ''
+        }
+        login_rst = self.session.post(login_url, data=fromData)
+        #logger.info('%s', login_rst.cookies)
+        if re.search(u'现在将转入登录前页面',login_rst.text):
+            logger.info('%s 登陆成功', username)
+        #    logger.info('%s', session.cookies.get_dict())
+        else:
+            logger.error('登陆失败')
+
+    def form_hash(self):
+        rst = self.session.get( self.url + '/member.php?mod=logging&action=login').text
+        loginhash = re.search(r'<div id="main_messaqge_(.+?)">', rst).group(1)
+        formhash = re.search(r'<input type="hidden" name="formhash" value="(.+?)" />', rst).group(1)
+        return loginhash, formhash
+
+    def checkin(self):
+        self.user_login()
+        #formhash使用一次失效。重新获取
+        headers = {
+             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36',
+        #     'cookie': '"dyHK_3864_auth": "9cecL5CZ9WTw%2ByyIpQIJKwmTEgJ4xeSgUhCV4uNjzrw9Vmr4cXusJt5ibbHWtNZuWOmc6uda0D3YcycCI4Vs4DHLTA", "dyHK_3864_checkfollow": "1", "dyHK_3864_lastact": "1650792261%09member.php%09logging", "dyHK_3864_lastcheckfeed": "75796%7C1650792261", "dyHK_3864_lastvisit": "1650788661", "dyHK_3864_lip": "101.34.119.114%2C1650792088", "dyHK_3864_saltkey": "kg774mcr", "dyHK_3864_sid": "OOJ9f3", "dyHK_3864_ulastactivity": "26f4SmdbhA%2FSueOJn84dbRTWNPeS0IM7N3vCvbrm2uyQPkPjL1U5"'
+         }
+        
+        rst = self.session.get( self.url + '/member.php?mod=logging&action=login').text
+        formhash = re.search(r'<input type="hidden" name="formhash" value="(.+?)" />', rst).group(1)
+        formData = {
+            'id': 'k_misign:sign',
+            'operation': 'qiandao',
+            'format': 'global_usernav_extra',
+            'formhash': formhash,
+            'inajax': '1',
+            'ajaxtarget': 'k_misign_topb'
+        }
+        checkin_url = self.url + '/plugin.php?id=k_misign:sign'
+        checkin_rst = self.session.post(checkin_url, headers=headers, data=formData)
+        logger.info('%s', checkin_rst.text)
+
+if __name__ == '__main__':
+    username = 'tanming'
+    password = 'zM@1314moxing'
+    if username is None or password is None:
+        logger.info('未配置登陆信息，尝试从环境变量获取……')
+        username = os.environ.get('MX_USERNAME', None)
+        password = os.environ.get('MX_PASSWORD', None)
+    MXcheckin(username, password).checkin()
---
+++ checkin/push.py
@@ -0,0 +1,109 @@
+import os
+import requests
+from configparser import ConfigParser
+import logging
+
+cfg = ConfigParser()
+
+
+def load_config():
+    config_path = os.path.join(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config'), 'push.ini')
+    if os.path.exists(config_path):
+        cfg.read(config_path, encoding='utf-8')
+        return True
+    else:
+        return False
+
+def title(checkin_name):
+    return " checkin_name 签到："
+
+
+# telegram的推送
+def telegram(checkin_name, push_message):
+    requests.post(
+        url="https://{}/bot{}/sendMessage".format(cfg.get('telegram', 'api_url'), cfg.get('telegram', 'bot_token')),
+        data={
+            "chat_id": cfg.get('telegram', 'chat_id'),
+            "text": title(status) + "\r\n" + push_message
+        }
+    )
+
+
+# server酱
+def ftqq(checkin_name, push_message):
+    requests.post(
+        url="https://sctapi.ftqq.com/{}.send".format(cfg.get('setting', 'push_token')),
+        data={
+            "title": title(checkin_name),
+            "desp": push_message
+        }
+    )
+
+
+# pushplus
+def pushplus(checkin_name, push_message):
+    requests.post(
+        url="https://www.pushplus.plus/send",
+        data={
+            "token": cfg.get('setting', 'push_token'),
+            "title": title(checkin_name),
+            "content": push_message
+        }
+    )
+
+
+# cq http协议的推送
+def cqhttp(checkin_name, push_message):
+    requests.post(
+        url=cfg.get('cqhttp', 'cqhttp_url'),
+        json={
+            "user_id": cfg.getint('cqhttp', 'cqhttp_qq'),
+            "message": title(checkin_name) + "\r\n" + push_message
+        }
+    )
+
+
+# 企业微信 感谢linjie5492@github
+def wecom(checkin_name, push_message):
+    secret = cfg.get('wecom', 'secret')
+    wechat_id = cfg.get('wecom', 'wechat_id')
+    push_token = requests.post(
+        url=f'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={wechat_id}&corpsecret={secret}',
+        data="").json()['access_token']
+    push_data = {
+        "agentid": cfg.get('wecom', 'agentid'),
+        "msgtype": "text",
+        "touser": "@all",
+        "text": {
+            "content": title(checkin_name) + "\r\n" + push_message
+        }, "safe": 0}
+    requests.post(f'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={push_token}', json=push_data)
+
+
+# pushdeer
+def pushdeer(checkin_name, push_message):
+    requests.get(
+        url=f'{cfg.get("pushdeer", "api_url")}/message/push',
+        params={
+            "pushkey": cfg.get("pushdeer", "token"),
+            "text": title(checkin_name),
+            "desp": str(push_message).replace("\r\n", "\r\n\r\n"),
+            "type": "markdown"
+        }
+    )
+
+
+def push(checkin_name, push_message):
+    if not load_config():
+        return 0
+    if cfg.getboolean('setting', 'enable'):
+        push_server = cfg.get('setting', 'push_server').lower()
+        log.info("正在执行推送......")
+        try:
+            log.debug(f"推送所用的服务为：{push_server}")
+            eval(push_server[:10].lower() + "(checkin_name, push_message)")
+        except NameError:
+            log.warning("推送服务名称错误")
+        else:
+            log.info("推送完毕......")
+    return 0
